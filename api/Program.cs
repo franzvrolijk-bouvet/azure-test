5f65aa127700b90200a4c5c25059cb3f4ee9d755
Franz Vrolijk <franz.vrolijk@bouvet.no>
2024-12-20 12:30:54 +0100

    WebApi deploy

--- api/Program.cs
+++ api/Program.cs
@@ -1,15 +1,25 @@
-var builder = WebApplication.CreateBuilder(args);
+using System.Text;
+using Azure.Identity;
+using Azure.Security.KeyVault.Secrets;
+using Azure.Storage.Blobs;
 
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+var builder = WebApplication.CreateBuilder(args);
 
 var app = builder.Build();
 
 app.UseHttpsRedirection();
 
-app.MapPost("", () =>
+app.MapPost("upload", async (string value, IConfiguration config) =>
 {
+    var keyVaultUri = new Uri(config["KeyVaultUri"] ?? throw new ApplicationException("KeyVaultUri missing"));
+    var kvClient = new SecretClient(keyVaultUri, new DefaultAzureCredential());
+    var blobStorageConnectionString = (await kvClient.GetSecretAsync("StorageAccountConnectionString")).Value.Value;
 
+    var blobContainerClient = new BlobContainerClient(blobStorageConnectionString, "items");
+    await blobContainerClient.CreateIfNotExistsAsync();
+
+    var blobClient = blobContainerClient.GetBlobClient(Guid.NewGuid().ToString());
+    await blobClient.UploadAsync(new MemoryStream(Encoding.UTF8.GetBytes(value)), false);
 });
 
 app.Run();
