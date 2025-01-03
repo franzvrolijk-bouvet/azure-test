7d037cff37531e307a98487f6119b078745cc2c7
Franz Vrolijk <franz.vrolijk@bouvet.no>
2025-01-03 10:57:08 +0100

    Function queue to cosmos

---
+++ functions/src/functions/queueToCosmos.ts
@@ -0,0 +1,31 @@
+import { app, InvocationContext } from "@azure/functions";
+import { CosmosClient } from "@azure/cosmos";
+import { randomUUID } from "crypto";
+
+export async function queueToCosmos(queueItem: unknown, context: InvocationContext): Promise<void> {
+  const dbName = "db";
+  const containerName = "container";
+
+  const connectionString = process.env.CosmosDbConnectionString;
+  if (!connectionString) {
+    context.log("Cosmos DB connection string is not defined in environment variables.");
+    return;
+  }
+
+  const client = new CosmosClient(connectionString);
+  const database = client.database(dbName);
+  const container = database.container(containerName);
+
+  try {
+    const { resource: createdItem } = await container.items.create({ id: randomUUID(), data: queueItem });
+    context.log(`Created item with id: ${createdItem.id}`);
+  } catch (error) {
+    context.log(`Error creating item: ${error}`);
+  }
+}
+
+app.storageQueue("queueToCosmos", {
+  queueName: "outqueue",
+  connection: "StorageAccountConnectionString",
+  handler: queueToCosmos,
+});
