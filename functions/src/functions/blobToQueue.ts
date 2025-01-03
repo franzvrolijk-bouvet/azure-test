b9fa5c6895f5186bb0ce4ef582aec47ee4d5e7e7
Franz Vrolijk <franz.vrolijk@bouvet.no>
2025-01-03 15:45:44 +0100

    Encoding

--- functions/src/functions/blobToQueue.ts
+++ functions/src/functions/blobToQueue.ts
@@ -1,24 +1,28 @@
 import { app, HttpResponseInit, InvocationContext } from "@azure/functions";
 import { QueueServiceClient } from "@azure/storage-queue";
 
-export async function PushToQueue(blob: Buffer, context: InvocationContext): Promise<HttpResponseInit> {
-  const queueName = "outqueue";
-  const connection = process.env.StorageAccountConnectionString;
-  try {
-    if (!connection) {
-      context.log("Storage account connection string is not defined in environment variables.");
-      return { status: 500, body: "Internal Server Error" };
-    }
+export async function pushToQueue(blob: Buffer, context: InvocationContext): Promise<HttpResponseInit> {
+  const blobData = blob.toString();
+  context.log("Received blob: ", blobData);
 
-    console.log("Creating queue service client");
+  const base64EncodedData = Buffer.from(blobData).toString("base64");
+  context.log("Base64 encoded data: ", base64EncodedData);
+
+  const queueName = "outqueue";
+  const connection = process.env.AzureWebJobsStorage;
+
+  if (!connection) {
+    context.log("Storage account connection string is not defined in environment variables.");
+    return { status: 500, body: "Internal Server Error" };
+  }
+
+  try {
     const queueServiceClient = QueueServiceClient.fromConnectionString(connection);
-    console.log("Creating queue client");
     const queueClient = queueServiceClient.getQueueClient(queueName);
-    console.log("Creating queue if not exists");
     await queueClient.createIfNotExists();
 
-    console.log(`Sending message to queue: ${blob.toString()}`);
-    await queueClient.sendMessage(blob.toString());
+    console.log(`Sending message to queue: ${base64EncodedData}`);
+    await queueClient.sendMessage(base64EncodedData);
     return { status: 200, body: "Message added to queue" };
   } catch (error) {
     context.log(`Error: ${error}`);
@@ -26,8 +30,8 @@ export async function PushToQueue(blob: Buffer, context: InvocationContext): Pro
   }
 }
 
-app.storageBlob("PushToQueue", {
+app.storageBlob("pushToQueue", {
   path: "items/{name}",
-  connection: "StorageAccountConnectionString",
-  handler: PushToQueue,
+  connection: "AzureWebJobsStorage",
+  handler: pushToQueue,
 });
--- functions/src/functions/queueToCosmos.ts
+++ functions/src/functions/queueToCosmos.ts
@@ -3,14 +3,14 @@ import { CosmosClient } from "@azure/cosmos";
 import { randomUUID } from "crypto";
 
 export async function queueToCosmos(queueItem: unknown, context: InvocationContext): Promise<void> {
-  console.log("Received queue item: ", queueItem);
   context.log("Received queue item: ", queueItem);
+
   const dbName = "db";
   const containerName = "container";
 
   const connectionString = process.env.CosmosDbConnectionString;
   if (!connectionString) {
-    console.log("Cosmos DB connection string is not defined in environment variables.");
+    context.log("Cosmos DB connection string is not defined in environment variables.");
     return;
   }
 
@@ -20,14 +20,14 @@ export async function queueToCosmos(queueItem: unknown, context: InvocationConte
 
   try {
     const { resource: createdItem } = await container.items.create({ id: randomUUID(), data: queueItem });
-    console.log(`Created item with id: ${createdItem.id}`);
+    context.log(`Created item with id: ${createdItem.id}`);
   } catch (error) {
-    console.log(`Error creating item: ${error}`);
+    context.log(`Error creating item: ${error}`);
   }
 }
 
 app.storageQueue("queueToCosmos", {
   queueName: "outqueue",
-  connection: "StorageAccountConnectionString",
+  connection: "AzureWebJobsStorage",
   handler: queueToCosmos,
 });
